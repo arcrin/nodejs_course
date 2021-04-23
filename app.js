fcd732a033d52be6c03484e570d9e72565ec8281
wbai <wbai@deltacontrols.com>
2021-04-23 16:15:13 -0700

    from work

--- app.js
+++ app.js
@@ -1,45 +1,25 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const rootDir = require('./util/path');
 
 
-const server = http.createServer((req, res) => {
-	const url = req.url;
-	const method = req.method;
-	if (url === '/') {
-		// res.setHeader('Content-Type', 'text/html')
-		res.write('<html>');
-		res.write('<head><title>Enter Message</title></head>');
-		res.write('<body><form action="/message" method="POST">' +
-			'<input type="text" name="message">' +
-			'<button type="submit">Send</button>' +
-			'</form></body>');
-		res.write('</html>');
-		return res.end();
-	}
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-	if (url === '/message' && method === 'POST') {
-		const body = [];
-		req.on('data', (chunk) => {
-			console.log(chunk);
-			body.push(chunk);
-		});
-		return req.on('end', () => {
-			const parsedBody = Buffer.concat(body).toString();
-			const message = parsedBody.split('=')[1];
-			fs.writeFile('message.txt', message, err => {
-				res.statusCode = 302;
-				res.setHeader('Location', '/');
-				return res.end();
-			});
-			// console.log(parsedBody);
-		});
-	}
-	res.setHeader('Content-Type', 'text/html')
-	res.write('<html>');
-	res.write('<head><title>My First Page</title></head>');
-	res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-	res.write('</html>');
-	res.end()
-});
+const app = express();
 
-server.listen(3000);
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin',adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
+})
+
+app.listen(3000)
